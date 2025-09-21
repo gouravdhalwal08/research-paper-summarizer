e3019080e8149dff8896a98852a25cb2a976daa2
Gourav Dhalwal <116418966+gouravdhalwal08@users.noreply.github.com>
2025-09-21 18:42:43 +0530

    Delete tests/test_pdf.py

--- tests/test_pdf.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from utils.pdf_reader import extract_text_from_pdf
-
-text = extract_text_from_pdf("data/sample.pdf")
-print(text[:1000])
